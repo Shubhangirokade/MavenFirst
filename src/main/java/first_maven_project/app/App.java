8eddd15a2852bb9723dd9eb904053b35a2f08142
Shubhangirokade <shubhangirokade97@gmail.com>
2023-10-09 14:37:39 +0530

    Initial commit

---
+++ src/main/java/first_maven_project/app/App.java
@@ -0,0 +1,13 @@
+package first_maven_project.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
